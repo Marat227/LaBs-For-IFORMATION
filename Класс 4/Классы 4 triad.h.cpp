1ee7ded4a3a4fafd3d598414e95ba3d9a5bcc4eb
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-04-25 01:12:39 +0500

    Add files via upload

---
+++ Класс 4/Классы 4 triad.h.cpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+class TRIAD
+{
+protected:
+	int first;
+	int second;
+	int third;
+public:
+	// конструкторы
+	TRIAD(int, int, int);
+	TRIAD();
+	TRIAD(TRIAD&);
+	~TRIAD();
+
+	// модификаторы
+	void setFirst(int);
+	void setSecond(int);
+	void setThird(int);
+
+	// селекторы
+	int getFirst();
+	int getSecond();
+	int getThird();
+
+	// перегрузка оператора присваивания 
+	TRIAD& operator= (const TRIAD& p);
+
+	// методы увеличения полей на 1
+	void first_1();
+	void second_1();
+	void third_1();
+
+
+	friend istream& operator>>(istream& in, TRIAD& t);
+	friend ostream& operator<<(ostream& out, const TRIAD& tmp);
+};
