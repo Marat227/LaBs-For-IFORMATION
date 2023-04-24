9c535ae0e98f50b77229e327587894da45de88d2
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-04-25 00:48:02 +0500

    Add files via upload

---
+++ 6 лаба/6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string s1;
+    std::string tempStr;
+    std::string spb = " ";
+    int n;
+
+    n = 0;
+
+    std::cout << "Введите строку" << std::endl;
+    std::getline(std::cin >> std::ws, s1);
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] == spb[0])
+        {
+            tempStr.assign(s1, n, i - n);
+            if ((s1.find(tempStr) == n) && (s1.rfind(tempStr) == n))
+            {
+                std::cout << "Слово " << tempStr << " встречается один раз" << std::endl;
+            }
+            n = i += 1;
+        }
+    }
+    tempStr.assign(s1, n, s1.size() - n);
+    if ((s1.find(tempStr) == n) && (s1.rfind(tempStr) == n))
+    {
+        std::cout << "Слово " << tempStr << " встречается один раз" << std::endl;
+    }
+    return 0;
+}
+
