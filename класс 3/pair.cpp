95db461260655f0d37b95aefc1d8b690ec164a54
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-05-26 04:57:29 +0500

    Add files via upload

---
+++ класс 3/pair.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "Pair.h"
+
+Pair::Pair() {
+	first = 0;
+	second = 0.0;
+}
+Pair::Pair(int a, double b) {
+	first = a;
+	second = b;
+}
+Pair::Pair(Pair& t) {
+	first = t.first;
+	second = t.second;
+}
+int Pair::getFirst() {
+	return first;
+}
+void Pair::setFirst(int tf) {
+	first = tf;
+}
+double Pair::getSecond() {
+	return second;
+}
+void Pair::setSecond(double tf) {
+	second = tf;
+}
+Pair& Pair::operator=(const Pair& ptr) {
+	if (&ptr == this) return *this;
+	first = ptr.first;
+	second = ptr.second;
+	return *this;
+}
+
+std::istream& operator>>(std::istream& in, Pair& p) {
+	std::cout << "First? " << std::endl;  in >> p.first;
+	std::cout << "Second?" << std::endl; in >> p.second;
+	return in;
+}
+std::ostream& operator<<(std::ostream& out, Pair& p) {
+	return out << p.first << ":" << p.second;
+}
+void Pair::show() {
+	std::cout << first << ":" << second << std::endl;
+}
+Pair& Pair::operator-(const Pair& a) {
+	first -= a.first;
+	second -= a.second;
+	return *this;
+}
---
+++ класс 3/pair.h
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+
+class Pair {
+private:
+	int first;
+	double second;
+public:
+	Pair();
+	Pair(int, double);
+	Pair(Pair&);
+	~Pair() {};
+
+	int getFirst();
+	void setFirst(int);
+	double getSecond();
+	void setSecond(double);
+
+	void show();
+
+	template <typename T>
+	Pair& operator+(const T);
+
+	Pair& operator-(const Pair&);
+	Pair& operator=(const Pair&);
+	friend std::istream& operator>>(std::istream&, Pair&);
+	friend std::ostream& operator<<(std::ostream&, Pair&);
+};
+
+template <typename T>
+Pair& Pair::operator+(const T p) {
+	if (p != (int)p) {
+		second += p;
+	}
+	else {
+		first += p;
+	}
+	return *this;
+}
