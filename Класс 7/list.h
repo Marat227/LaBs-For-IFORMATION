a24521efcad4b15cd0e0122ff71ac9a66599f94a
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-05-26 04:58:51 +0500

    Add files via upload

---
+++ Класс 7/list.h
@@ -0,0 +1,195 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+template <class dataType>
+class List;
+
+template <class dataType>
+class Node;
+
+template <class dataType>
+class Node
+{
+	friend class List<dataType>;
+
+public:
+	Node();
+	Node(dataType);
+	~Node();
+
+	dataType getData();
+	Node* getPtr();
+private:
+	dataType data;
+	Node* ptrNext;
+};
+
+template <class dataType>
+class List
+{
+public:
+	List();
+	List(dataType);
+	List(const List<dataType>&);
+	~List();
+
+	dataType& operator[](int);
+
+	List<dataType>& operator=(const List<dataType>&);
+	size_t operator()();
+
+	friend List<dataType>& operator*(List<dataType>& sList, List<dataType>& fList)
+	{
+		List<dataType> tmpList;
+		int maxSize = fList.size > sList.size ? fList.size : sList.size;
+		int minSize = fList.size <= sList.size ? fList.size : sList.size;
+
+		for (int i = 0; i < minSize; i++)
+		{
+			dataType a = fList[i];
+			dataType b = sList[i];
+			tmpList.push(a * b);
+		}
+
+		return tmpList;
+	}
+	friend std::ostream& operator<<(std::ostream& out, const List<dataType>& tList)
+	{
+		Node<dataType>* curr = tList.head;
+		for (int i = 0; i < tList.size; i++)
+		{
+			out << curr->getData() << " ";
+			curr = curr->getPtr();
+		}
+		return out;
+	}
+	friend std::istream& operator>>(std::istream& in, List<dataType>& tList)
+	{
+		dataType a;
+		in >> a;
+		tList.push(a);
+		return in;
+	}
+
+	void push(dataType);
+
+private:
+	Node<dataType>* head;
+	size_t size;
+};
+
+template <class dataType>
+Node<dataType>::Node()
+{
+	// data = 0;
+	ptrNext = nullptr;
+}
+template <class dataType>
+Node<dataType>::Node(dataType data)
+{
+	this->data = data;
+	ptrNext = nullptr;
+}
+template <class dataType>
+Node<dataType>::~Node()
+{
+	if (ptrNext != nullptr)
+	{
+		delete ptrNext;
+	}
+}
+template <class dataType>
+dataType Node<dataType>::getData()
+{
+	return data;
+}
+template <class dataType>
+Node<dataType>* Node<dataType>::getPtr()
+{
+	return ptrNext;
+}
+
+/// <summary>
+/// Node* head;
+/// </summary>
+template <class dataType>
+List<dataType>::List()
+{
+	head = nullptr;
+	size = 0;
+}
+template <class dataType>
+List<dataType>::List(dataType data)
+{
+	head = new Node<dataType>(data);
+	size = 1;
+}
+template <class dataType>
+List<dataType>::List(const List<dataType>& tList)
+{
+	Node<dataType>* curr = tList.head;
+	while (curr != nullptr)
+	{
+		dataType a = curr->data;
+		this->push(a);
+		curr = curr->ptrNext;
+	}
+}
+template <class dataType>
+List<dataType>::~List()
+{
+	if (head != nullptr)
+	{
+		delete head;
+		head = head->ptrNext;
+	}
+	size = 0;
+}
+template <class dataType>
+void List<dataType>::push(dataType data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<dataType>(data);
+	}
+	else
+	{
+		Node<dataType>* curr = head;
+		while (curr->ptrNext != nullptr)
+		{
+			curr = curr->ptrNext;
+		}
+		curr->ptrNext = new Node<dataType>(data);
+	}
+	size++;
+}
+template <class dataType>
+dataType& List<dataType>::operator[](int indx)
+{
+	Node<dataType>* curr = head;
+	for (int i = 0; i < indx; i++)
+	{
+		curr = curr->ptrNext;
+	}
+	return curr->data;
+}
+template <class dataType>
+size_t List<dataType>::operator()()
+{
+	return size;
+}
+
+template <class dataType>
+List<dataType>& List<dataType>::operator=(const List<dataType>& tList)
+{
+	Node<dataType>* curr = tList.head;
+	while (curr != nullptr)
+	{
+		dataType a = curr->data;
+		this->push(a);
+		curr = curr->ptrNext;
+	}
+	return *this;
+}
---
+++ Класс 7/main.cpp
@@ -0,0 +1,49 @@
+﻿#include "Header.h"
+#include "Pair.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	List<int> listInt;
+
+	List<Pair> listPair;
+
+	Pair c;
+
+	listPair.push(c);
+	c.first = 3;
+	c.second = 5.5;
+	listPair.push(c);
+
+	cout << "listPair: " << listPair << endl;
+
+	cout << "Enter first elem in da listPair" << endl;
+	cin >> listPair[1];
+	cout << "listPair[1] " << listPair[1] << endl;
+
+
+	listInt.push(3);
+	listInt.push(5);
+
+	cout << "listInt: " << listInt << endl;
+
+	List<double> listDouble;
+
+	listDouble.push(4.5);
+	listDouble.push(6.4);
+
+	cout << "listDouble: " << listDouble << endl;
+
+	cout << "Enter elements in da list double" << endl;
+	cin >> listDouble;
+	cout << listDouble;
+
+	return 0;
+}
+
+void listFillFunction(List& tList, int n) {
+	for (int i = 0; i < n; i++) {
+		cin >> tList;
+	}
+}
