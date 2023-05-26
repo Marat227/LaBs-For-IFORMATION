80ee8ca801a67f10131549e4ea1a9033c1e6a207
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-05-26 13:02:45 +0500

    Add files via upload

---
+++ Сортировки/puzir.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+void bubbleSort(int list[], int listLength)
+{
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
+		{
+			if (list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+}
+
+int main()
+{
+	int list[5] = { 3,19,8,0,48 };
+	cout << "Input array ..." << endl;
+	for (int i = 0; i < 5; i++)
+		cout << list[i] << '\t';
+	cout << endl;
+
+	bubbleSort(list, 5);
+
+	cout << "Sorted array ..." << endl;
+	for (int i = 0; i < 5; i++)
+		cout << list[i] << '\t';
+	cout << endl;
+}
