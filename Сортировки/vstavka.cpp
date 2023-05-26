f0ece748d54b010d2da832e0ece081c1dd82c1fd
МаРаТИкСС <131374311+Marat227@users.noreply.github.com>
2023-05-26 13:06:17 +0500

    Add files via upload

---
+++ Сортировки/vstavka.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void insertionSort(int list[], int listLength)
+{
+	for (int i = 1; i < listLength; i++)
+	{
+		int j = i - 1;
+		while (j >= 0 && list[j] > list[j + 1])
+		{
+			swap(list[j], list[j + 1]);
+			cout << "\ndid";
+			j--;
+		}
+	}
+}
+
+int main()
+{
+	int list[8] = { 3,19,8,0,48,4,5,12 };
+	cout << "Input array ...\n";
+	for (int i = 0; i < 8; i++)
+	{
+		cout << list[i] << "\t";
+	}
+
+	insertionSort(list, 8);
+
+	cout << "\n\nSorted array ... \n";
+	for (int i = 0; i < 8; i++)
+	{
+		cout << list[i] << "\t";
+	}
+
+	return 0;
+}
